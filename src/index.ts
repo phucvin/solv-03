f8df85fd27faf926dd11b5157f101ca555c4f8f1
phucvin <phucvin@gmail.com>
2025-11-02 13:55:53 -0800

    add types

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import { createServer } from 'node:http';
+import { IncomingMessage, ServerResponse, createServer } from 'node:http';
 
 import counter01 from './routes/counter01';
 
 const server = createServer(async (req, res) => {
-	const routes = {
-		'/counter01': counter01,
-	};
-	if (req.url === '/') {
+	const routes = new Map<string, (req: IncomingMessage, res: ServerResponse) => Promise<void>>([
+		['/counter01', counter01],
+	]);
+	if (!req.url || req.url === '/') {
 		res.writeHead(200, { 'Content-Type': 'text/html' });
 		let index = '<h1>Index</h1>';
-		for (const route in routes) {
+		for (const [route, _] of routes.entries()) {
 			index += `<h2><a href="${route}">${route.slice(1)}</a></h2>`;
 		}
 		res.end(index);
-	} else if (req.url && routes[req.url]) {
-		await routes[req.url](req, res);
+	} else if (routes.has(req.url)) {
+		await (routes.get(req.url)!(req, res));
 	} else if (req.url === '/api/status') {
 		res.writeHead(200, { 'Content-Type': 'application/json' });
 		res.end(JSON.stringify({ status: 'ok', timestamp: Date.now() }));
