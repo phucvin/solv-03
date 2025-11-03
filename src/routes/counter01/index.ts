9da56272d807dafec8a47699da173ac1b367c0cf
phucvin <phucvin@gmail.com>
2025-11-02 19:56:33 -0800

    refactor

--- src/routes/counter01/index.ts
+++ src/routes/counter01/index.ts
@@ -1,17 +1,9 @@
-import { IncomingMessage, ServerResponse } from "node:http";
-import { Solv, render } from "../../server";
+import { Solv } from "../../server";
 import { BODY, DOCUMENT } from "../../shared";
 
-async function App(solv: Solv) {
+export default async function (solv: Solv) {
     const hello = solv.newElement('span');
     hello.setValue('innerHTML', 'hello');
     solv.getElement(BODY).setChildren([hello.id]);
     solv.getElement(DOCUMENT).setValue('title', 'hello');
 }
-
-async function serve(req: IncomingMessage, res: ServerResponse) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(await render(App));
-}
-
-export default serve;
