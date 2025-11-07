10e22762d64f25430c96959550109afba9f485cf
phucvin <phucvin@gmail.com>
2025-11-06 16:20:38 -0800

    fix cid type

--- src/ssr.ts
+++ src/ssr.ts
@@ -1,6 +1,6 @@
 import { CommandMap } from "./shared";
 
-export default function (cid: number, cm: CommandMap): string {
+export default function (cid: string, cm: CommandMap): string {
     let s = `
 <html>
     <head>
@@ -10,7 +10,7 @@ export default function (cid: number, cm: CommandMap): string {
     <script type="module">
         import '/client.mjs';
         import './index_handlers.mjs';
-        globalThis.SOLV_CID = ${cid};
+        globalThis.SOLV_CID = '${cid}';
         solv.applyCommandMap(JSON.parse(\`\n${JSON.stringify(cm, null, 2)}\n\`));
     </script>
 </html>
